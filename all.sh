31f969cb3e2189f7ba13038e35116570284eaba6
Kevin Mader <kevinmader@gmail.com>
2014-10-08 17:30:12 +0200

    updating files and installation script

--- all.sh
+++ all.sh
@@ -7,12 +7,15 @@ module load new
 module load java
 module load open_mpi
 
+setup_spark.sh
+
 python path_to/start_spark_euler.py -c 8 -m 20g 
 # creates the slaves file, starts the spark master and worker processes using mpirun
 # the "-c" option specifies number of cores per worker
 # -m specifies SPARK_MEMORY
 
-spark/bin/spark-submit --class "YourMain" --master spark://$HOSTNAME:7077 path_to_your_jar.jar args
+$SPARK_HOME/bin/spark-submit --class "YourMain" --master spark://$HOSTNAME:7077 path_to_your_jar.jar args
 
-SPARK_SLAVES=$HOME/slaves_$LSB_JOBID spark/sbin/stop-all.sh
-spark/sbin/stop-slaves.sh
+SPARK_SLAVES=$HOME/slaves_$LSB_JOBID
+$SPARK_HOME/sbin/stop-all.sh
+$SPARK_HOME/sbin/stop-slaves.sh
