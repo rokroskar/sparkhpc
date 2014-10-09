db7f2c8e322b4c7cb0a6e34622ebee1e2ed165e8
Kevin Mader <kevinmader@gmail.com>
2014-10-09 11:35:21 +0000

    fixed paths and removed cwd which is apparently done automatically

--- all.sh
+++ all.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #BSUB -J sparkpi_job
 #BSUB -W 00:10 # requesting 10 minutes
-#BSUB -o sparkpi.log
-#BSUB -e sparkpi.err
-#BSUB -cwd "$(pwd)" # set the directory to the current one
+#BSUB -oo sparkpi.log # output extra o means overwrite
+#BSUB -eo sparkpi.err
+#BSUB -R "rusage[scratch=1000]" # 1000MB per core
 #BSUB -n 24 # requesting 24 cores -- request in multiples of 24 to get entire nodes
 
 module load new
@@ -11,7 +11,7 @@ module load java
 module load open_mpi
 
 # setup the spark path
-. setup_spark.sh
+. ./setup_spark.sh
 
 # initialize the nodes
 python start_spark_euler.py -c 8 -m 20g 
@@ -23,10 +23,11 @@ python start_spark_euler.py -c 8 -m 20g
 # the specific example runs spark's pi estimation with a slices = 100 (first and only argument)
 
 echo " Master is set as $HOSTNAME"
+JARFILE="$SPARK_HOME/lib/spark-examples*.jar" # version depends on spark version
 
 $SPARK_HOME/bin/spark-submit --class org.apache.spark.examples.SparkPi --master \
-    spark://$HOSTNAME:7077 $SPARK_HOME/lib/spark-examples-1.1.0-hadoop2.4.0.jar \
-    100
+    spark://$HOSTNAME:7077 $JARFILE \
+    1000
 
 SPARK_SLAVES=$HOME/slaves_$LSB_JOBID
 $SPARK_HOME/sbin/stop-all.sh
