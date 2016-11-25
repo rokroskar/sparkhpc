58dfefd8e80cb9e19c6b6d42214c778d89e2845c
Rok Roskar <rokroskar@gmail.com>
2016-11-25 18:15:51 +0100

    implemented html printing of job and cluster properties in the jupyter notebook; implemented the option to initialize SparkJob with a cluster id instead of job id

--- sparkhpc/sparkjob.py
+++ sparkhpc/sparkjob.py
@@ -19,6 +19,13 @@ import pkg_resources
 import logging
 import signal
 
+try: 
+    get_ipython()
+    IPYTHON=True
+    from IPython.display import display, HTML
+except NameError: 
+    IPYTHON=False
+
 class bc:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -59,8 +66,16 @@ class SparkJob(object):
     See the LSFSparkJob class for an example.
     """
 
+    table_header = """
+                    <th>Job ID</th>
+                    <th>Status</th>
+                    <th>Spark UI</th>
+                    <th>Spark URL</th>
+                    """
+
     def __init__(self, 
                 jobid=None,
+                clusterid=None,
                 jobname='sparkcluster', 
                 ncores='4', 
                 walltime='00:30', 
@@ -70,6 +85,13 @@ class SparkJob(object):
                 follow_up_script="",
                 spark_home=None):
         
+        if clusterid is not None:
+            sjs = self.current_clusters()
+            if clusterid < len(sjs): 
+                jobid = sjs[clusterid].jobid
+            else: 
+                logger.error('cluster %d does not exist'%clusterid)
+                
         # try to load JSON data for the job
         if jobid is not None: 
             jobid = str(jobid)
@@ -78,6 +100,7 @@ class SparkJob(object):
                     self.prop_dict = json.load(f)
             except Exception as e: 
                 raise(e)
+
         else:
             if spark_home is None: 
                 spark_home = os.environ.get('SPARK_HOME', os.path.join(os.path.expanduser('~'),'spark'))   
@@ -99,20 +122,39 @@ class SparkJob(object):
 
         signal.signal(signal.SIGINT, self._sigint_handler)
 
+    def _repr_html_(self): 
+        table_header = "<tr>"+self.table_header+"</tr>"
+        return table_header + self._to_string()
+
     def __getattr__(self, val): 
         if val in self.prop_dict: 
             return self.prop_dict[val]
         else: 
             raise AttributeError
 
+    @property
+    def master_url(self): 
+        """Get the URL of the Spark master"""
+        return self._master_url(self.jobid)
+
+
+    @property
+    def master_ui(self): 
+        """Get the UI address of the Spark master"""
+        return self._master_ui(self.jobid)
+
+
     def _dump_to_json(self):
         """Write the data to recreate this SparkJob to a JSON file"""
+
         filename = os.path.join(home_dir, '.sparkhpc%s'%self.jobid)
         with open(filename, 'w') as fp:
             json.dump(self.prop_dict, fp)
 
+
     def wait_to_start(self, timeout=60):
         """Wait for the job to start or until timeout, whichever comes first"""
+
         if self.status is not 'submitted':
             self.submit()
 
@@ -122,41 +164,53 @@ class SparkJob(object):
                 break
             time.sleep(1)
 
-    def master_url(self): 
-        """Get the URL of the Spark master"""
-        return self._master_url(self.jobid)
 
     @classmethod
-    def _master_url(cls, jobid): 
+    def _master_url(cls, jobid, timeout=60): 
         """Retrieve the spark master address for jobid"""
+
         if cls._job_started(jobid): 
-            job_peek = subprocess.check_output([cls._peek_command, str(jobid)])
-            master_url = re.findall('(spark://\S+:\d{4})', job_peek)
+            timein = time.time()
+            while time.time() - timein < timeout:
+                job_peek = subprocess.check_output([cls._peek_command, str(jobid)])
+                master_url = re.findall('(spark://\S+:\d{4})', job_peek)
+                if len(master_url) > 0: 
+                    break
+                else:
+                    time.sleep(0.5)
+        
             if len(master_url) == 0: 
                 raise RuntimeError('Unable to obtain information about Spark master -- are you sure it is running?')
-            return master_url[0]
+            else:
+                return master_url[0]
         else: 
-            logger.info('Job %s not yet started'%jobid)
-
-
-    def master_ui(self): 
-        """Get the UI address of the Spark master"""
-        return self._master_ui(self.jobid)
+            return None
 
     @classmethod
-    def _master_ui(cls, jobid): 
+    def _master_ui(cls, jobid, timeout=60): 
         """Retrieve the web UI address for jobid"""
+
         if cls._job_started(jobid): 
-            job_peek = subprocess.check_output([cls._peek_command, str(jobid)])
-            master_ui = re.findall('(http://\S+:\d{4})', job_peek)
+            timein = time.time()
+            while time.time() - timein < timeout: 
+                job_peek = subprocess.check_output([cls._peek_command, str(jobid)])
+                master_ui = re.findall('(http://\S+:\d{4})', job_peek)
+                if len(master_ui) > 0:
+                    break
+                else: 
+                    time.sleep(0.5)
+    
             if len(master_ui) == 0: 
                 raise RuntimeError('Unable to obtain information about Spark master -- are you sure it is running?')
-            return master_ui[0]
-        else: 
-            logger.info('Job %s not yet started'%jobid)
+            else: 
+                return master_ui[0]
+        else:
+            return None
+
 
     def submit(self): 
         """Write job file to current working directory and submit to the scheduler"""
+
         # check that the user has setup the java environment
         if 'JAVA_HOME' not in os.environ:
             raise RuntimeError('JAVA_HOME not set - please set it to the location of your java installation')
@@ -182,31 +236,45 @@ class SparkJob(object):
             jobfile.write(job)
 
         self.prop_dict['jobid'] = self._submit_job('job')
+        self.prop_dict['status'] = 'submitted'
         self._dump_to_json()
 
         sjs = self.current_clusters()
         logger.info('Submitted cluster %d'%(len(sjs)-1))
 
+
     @classmethod
     def _submit_job(cls, jobfile): 
         """Submits the jobfile and returns the job ID"""
+
         job_submit = subprocess.Popen(cls._submit_command%jobfile, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        jobid = re.findall(cls._job_regex, job_submit.stdout.read())[0]
+        try: 
+            jobid = re.findall(cls._job_regex, job_submit.stdout.read())[0]
+        except Exception as e: 
+            logger.error('Job submission failed or jobid invalid')
+            raise e
         return jobid
 
 
     def stop(self): 
         """Stop the current job"""
         self._stop(self.jobid)
+        self.prop_dict['status'] = 'stopped'
+
 
     @classmethod
     def _stop(cls, jobid):
         out = subprocess.check_output([cls._kill_command, jobid], stderr=subprocess.STDOUT)
         logger.info(out)
 
+
     def job_started(self): 
         """Check whether the job is running already or not"""
-        return self._job_started(self.jobid)
+        started = self._job_started(self.jobid)
+        if started: 
+            self.prop_dict['status'] = 'running'
+        return started
+
 
     @classmethod 
     def _job_started(cls, jobid): 
@@ -214,6 +282,7 @@ class SparkJob(object):
         command.append(str(jobid))
         stat = subprocess.check_output(command).split('\n')
         return stat[1].split()[1] == 'RUN'
+
 
     @classmethod
     def current_clusters(cls):
@@ -230,6 +299,42 @@ class SparkJob(object):
                 sjs.append(LSFSparkJob(jobid=jobid))
         
         return sjs
+
+    def _to_string(self): 
+        if self.job_started(): 
+            self.prop_dict['status'] = 'running'
+
+        if IPYTHON:
+            row = """
+                    <td>{jobid}</td>
+                    <td>{status}</td>
+                    <td><a target="_blank" href="{ui}">{ui}</a></td>
+                    <td>{url}</td>
+                  """
+            
+        else:
+            row = "Job id: {jobid}\nStatus: {status}\nSpark UI: {ui}\nSpark URL: {url}"
+
+        return row.format(jobid=self.jobid, status=self.status, ui=self.master_ui, url=self.master_url)
+
+
+    def show_clusters(self): 
+        sjs = self.current_clusters()
+
+        if len(sjs) == 0: 
+            print('No Spark clusters found')
+
+        else:
+            if IPYTHON:
+                table_header = "<tr><td>ClusterID</td>"+self.table_header+"</tr>"
+                table_rows = ""
+                for i,sj in enumerate(sjs):
+                    table_rows += "<tr>"+"<td>%s</td>"%i+sj._to_string()+"</tr>"
+                display(HTML(table_header+table_rows))
+            else: 
+                for i,sj in enumerate(sjs): 
+                    print('----- Cluster %d -----'%i)
+                    print(sj)
 
     def _sigint_handler(self, signal, frame): 
         """Handle ctrl-c from the user"""
