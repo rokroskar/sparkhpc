34fd27f050aabb20daad1f71bb6959fb7f9badca
Rok Roskar <rokroskar@gmail.com>
2016-11-28 14:05:27 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,11 @@ setup(name="sparkhpc",
       author="Rok Roskar",
       author_email="roskar@ethz.ch",
       url="http://sparkhpc.readthedocs.io",
+      description="spark deployment on hpc resources made easy",
       package_dir={'sparkhpc/':''},
       packages=['sparkhpc'],
       scripts=['scripts/sparkcluster', 'scripts/hpcnotebook'],
-      include_package_data=True
+      include_package_data=True,
+      install_requires=['click'],
+      keywords=['pyspark', 'spark', 'hpc']
 )
