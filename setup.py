b9a1521b7bd80adec84655e2c5c82f3f51ab99c0
Rok Roskar <rokroskar@gmail.com>
2016-11-28 13:38:19 +0100

    version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="sparkhpc",
-      version='0.0.1',
+      version='0.1',
       author="Rok Roskar",
       author_email="roskar@ethz.ch",
       package_dir={'sparkhpc/':''},
