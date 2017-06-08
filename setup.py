de9f460f413aaacbb2829445cbf2377397d0e8fe
Rok Roskar <roskarr@ethz.ch>
2017-06-08 14:12:20 +0200

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="sparkhpc",
-      version='0.3.post3',
+      version='0.3.post4',
       author="Rok Roskar",
       author_email="roskar@ethz.ch",
       url="http://sparkhpc.readthedocs.io",
